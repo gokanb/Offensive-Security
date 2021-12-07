28fc72f7c29e6a004cab2e8bbefbf651ed3178fe
gokanb <gokan.bektas90@gmail.com>
2021-12-07 11:37:48 -0500

    .

---
+++ Offensive-Security/Http programming/HTTP Server/request_handler.py
@@ -0,0 +1,15 @@
+
+
+from http.server import BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+    """Custom request handler"""
+
+    def do_GET(self):
+        """Handler for the Get requests"""
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        #send the message to browser
+        self.wfile.write("Hello from server!")
+        return
