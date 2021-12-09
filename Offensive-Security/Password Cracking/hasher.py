91c3b8f057b7eaa5eb9cc69b902dac43d69ff311
Gokanb <gokan.bektas90@gmail.com>
2021-12-09 15:45:03 -0500

    .

--- Offensive-Security/Password Cracking/hasher.py
+++ Offensive-Security/Password Cracking/hasher.py
@@ -15,16 +15,16 @@ print(colored('Text: ' + hashvalue + ' with the md5-hashed is converted to: ' + 
 
 hash_object1 = hashlib.sha1()
 hash_object1.update(hashvalue.encode())
-print(colored('Text: ' + hashvalue + ' with the md5-hashed is converted to: ' + hash_object1.hexdigest(), 'red'))
+print(colored('Text: ' + hashvalue + ' with the sha1-hashed is converted to: ' + hash_object1.hexdigest(), 'red'))
 
 hash_object1 = hashlib.sha224()
 hash_object1.update(hashvalue.encode())
-print(colored('Text: ' + hashvalue + ' with the md5-hashed is converted to: ' + hash_object1.hexdigest(), 'yellow'))
+print(colored('Text: ' + hashvalue + ' with the sha224-hashed is converted to: ' + hash_object1.hexdigest(), 'magenta'))
 
 hash_object1 = hashlib.sha256()
 hash_object1.update(hashvalue.encode())
-print(colored('Text: ' + hashvalue + ' with the md5-hashed is converted to: ' + hash_object1.hexdigest(), 'white'))
+print(colored('Text: ' + hashvalue + ' with the sha256-hashed is converted to: ' + hash_object1.hexdigest(), 'white'))
 
 hash_object1 = hashlib.sha512()
 hash_object1.update(hashvalue.encode())
-print(colored('Text: ' + hashvalue + ' with the md5-hashed is converted to: ' + hash_object1.hexdigest(), 'cyan'))
+print(colored('Text: ' + hashvalue + ' with the sha512-hashed is converted to: ' + hash_object1.hexdigest(), 'cyan'))
