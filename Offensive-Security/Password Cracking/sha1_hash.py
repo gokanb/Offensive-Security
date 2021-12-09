e2477721ea399485481995fa06a2ba2188797c72
Gokanb <gokan.bektas90@gmail.com>
2021-12-09 15:53:46 -0500

    .

---
+++ Offensive-Security/Password Cracking/sha1_hash.py
@@ -0,0 +1,22 @@
+#!/ur/bin/env python
+
+# Author:Gokan Bektas
+# Description: 
+
+from urllib.request import urlopen
+import hashlib
+from termcolor import colored
+
+sha1hash = input('[*] Enter sha1 Hash value: ')
+
+passlist = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt'))
+
+for i in passlist.split('\n'):
+    hashguess = hashlib.sha1(bytes(i, 'utf-8')).hexdigest()
+    if hashguess == sha1hash:
+        print(colored('[+] the password is: ' + str(i), 'white'))
+        quit()
+    else:
+        print(colored('[-] Password guess ' + str(i) + ' does not match, trying next...', 'red'))
+        
+print('Password not in the password list!')
