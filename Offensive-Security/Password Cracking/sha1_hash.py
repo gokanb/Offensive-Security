cd33a6030b5f10c98f8d94521176b51ea87202ce
Gokanb <gokan.bektas90@gmail.com>
2021-12-09 16:09:34 -0500

    .

--- Offensive-Security/Password Cracking/sha1_hash.py
+++ Offensive-Security/Password Cracking/sha1_hash.py
@@ -9,14 +9,27 @@ from termcolor import colored
 
 sha1hash = input('[*] Enter sha1 Hash value: ')
 
-passlist = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt'))
+# passlist = str(urlopen("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"))
 
-for i in passlist.split('\n'):
-    hashguess = hashlib.sha1(bytes(i, 'utf-8')).hexdigest()
-    if hashguess == sha1hash:
-        print(colored('[+] the password is: ' + str(i), 'white'))
-        quit()
-    else:
-        print(colored('[-] Password guess ' + str(i) + ' does not match, trying next...', 'red'))
+# for i in passlist.split('\n'):
+#     hashguess = hashlib.sha1(bytes(i, 'utf-8')).hexdigest()
+#     if hashguess == sha1hash:
+#         print(colored('[+] the password is: ' + str(i), 'white'))
+#         quit()
+#     else:
+#         print(colored('[-] Password guess: ' + str(i) + ' does not match, trying next...', 'red'))
         
-print('Password not in the password list!')
+# print('Password not in the password list!') 
+
+file = open('passwords.txt', 'r')
+for password in file.readlines():
+    password = password.strip('\n')
+    hashguess = hashlib.sha1(bytes(password, 'utf-8')).hexdigest()
+    if hashguess == sha1hash:
+        print(colored('[+] the password is: ' + str(password), 'white'))
+    else:
+        print(colored('[-] Password guess: ' + str(password) + ' does not match, trying next...', 'red'))
+        
+print('Password not in the password list!') 
+
+        
