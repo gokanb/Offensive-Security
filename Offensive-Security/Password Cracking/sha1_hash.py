481b7adc4ca18d10c436e423855f86ef2e2a545c
Gokanb <gokan.bektas90@gmail.com>
2021-12-10 15:48:31 -0500

    .

--- Offensive-Security/Password Cracking/sha1_hash.py
+++ Offensive-Security/Password Cracking/sha1_hash.py
@@ -1,12 +1,14 @@
 #!/ur/bin/env python
 
 '''
-
 # Author: Gokan Bektas
 # Description:
                         ########################## Educational Purpose !!! ##########################
-# This script will take sha1 value after  
+ 
+            --- Before running this application make sure that first, you hash one of the decrypt value from given list 
+            from passwords.txt then take that sha1_hash value output and run sha1_hash.py and put output value as a input. ---
 
+# This script will take encrypted sha1hash value and found the decrypted value in given list passwords.txt and print out. 
 '''
 
 # from urllib.request import urlopen
@@ -16,7 +18,6 @@ from termcolor import colored
 sha1hash = input('[*] Enter sha1 Hash value: ')
 
 # passlist = str(urlopen("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"))
-
 # for i in passlist.split('\n'):
 #     hashguess = hashlib.sha1(bytes(i, 'utf-8')).hexdigest()
 #     if hashguess == sha1hash:
