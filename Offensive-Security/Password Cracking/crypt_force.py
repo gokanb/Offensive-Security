0e7d31d3bac47c374ac63202d70a59ba10ece794
Gokanb <gokan.bektas90@gmail.com>
2021-12-10 16:27:40 -0500

    .

--- Offensive-Security/Password Cracking/crypt_force.py
+++ Offensive-Security/Password Cracking/crypt_force.py
@@ -1,32 +1,30 @@
 #!/ur/bin/env python
 
 '''
-
 # Author: Gokan Bektas
 # Description:
                          ########################## Educational Purpose !!! ##########################
-# This script will use bruteforce to crack username and passwords thru using dictonary.txt store and pass1.txt store! 
-# Script have couple of python libraries. Crytp crypt. crypt(password) will return the hash of password. 
-# You store the hash instead of the clear text password. 
-# That way, you can't lose the password to a hacker because you don't have it.
+                         
+# This script will use bruteforce to find the password. will find and print out decrypted password in given list dictionary.txt 
 
+---pass1.txt stores username and password. 
+---dictionary.txt stores password.
 '''
 
 # importing libraries
 import crypt                        
 from termcolor import colored       
 
-# Function 
 def crackedPass(cryptWord): 
-    salt = cryptWord[0:2]                                                   
-    dictionary = open ('dictionary.txt', 'r')                               
+    salt = cryptWord[0:2]                      #                                       
+    dictionary = open ('dictionary.txt', 'r')  # open dictionary.txt   'r' refers for read.                       
     for word in dictionary.readlines():
         word = word.strip('\n')
         cryptPass = crypt.crypt(word, salt)
         if (cryptWord == cryptPass):
-            print(colored('[+] Found password: ' + word, 'white'))
+            print(colored('[+] Found password: ' + word, 'red'))
             return
-    print(colored('[-] Password not found! ', 'red'))
+    print(colored('[-] Password not found! ', 'white'))
     return
 
 def main():
